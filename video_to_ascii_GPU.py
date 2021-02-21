66b4428e05e8bbf9b5b8c09b963a1f493386d3ba
RishiGU <rishabh.20scse1010642@galgotiasuniversity.edu.in>
2021-02-21 14:38:07 +0530

    new updates

--- video_to_ascii_GPU.py
+++ video_to_ascii_GPU.py
@@ -3,8 +3,9 @@ import cv2,os,numpy as np
 import concurrent.futures
 from threading import Thread
 from time import perf_counter,sleep as nap
+from numba import jit 
+# may add sound later .
 
-# may add sound later .\
 class ascii_video :
     """ working of class 
             extract image and yield  
@@ -58,7 +59,6 @@ class ascii_video :
         else : self.steps = 1
         self.fps =int(default_fps/self.steps)
         print("new fps of video is --> ",self.fps)
-        self.reader_completed = False
         # extracting first frame for the setup 
         success,frame = self.vidcap.read()
         self.width,self.height = tuple(list(frame.shape)[0:2][::-1]) # for creating ascii from the image
@@ -79,22 +79,19 @@ class ascii_video :
         print(f"\nSaving video as - { self.video_output_name }")
         self.writer.release()
     
+    @jit
     def iter_each_frame(self):  
         success = True
-        t1 = Thread(target = lambda : None )
-        t1.start()
         while success:
             count = int(self.vidcap.get(1))
             success,frame = self.vidcap.read()
             if count%self.steps == 0 and success :
                 if success and self.total_frames > count : 
                     print(f"Working on frame -> '{str(count).zfill(5)}'")
-                    t1.join()
-                    t1 = Thread(target = lambda : self.frame_list.append(frame))
-                    t1.start()
-                    # make it save frames in thread  in frame list
-        self.reader_completed = True
+                    image = self.frame_to_ascii_to_ascii_image(frame)
+                    self.add_ascii_frame(image)
 
+    @jit
     def image_to_ascii_convertor(self,image):
         # read the image in the b&w format transpose it and return the ascii nested list for that 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).transpose()
@@ -111,6 +108,7 @@ class ascii_video :
 		
         return ascii_in_pixles
 
+    @jit
     def frame_to_ascii_to_ascii_image(self,current_frame):
         # take frame extract ascii data and return the ascii image
         # print('converting to ASCII images' ,end = " - ")
@@ -125,40 +123,19 @@ class ascii_video :
                     image = cv2.putText(image,ascii_val,(index_c*self.pbs,(index_r+1)*self.pbs),cv2.FONT_HERSHEY_PLAIN,0.9,(255,255,255),1)
         return image
     
+    @jit
     def add_ascii_frame(self,frame):
         # convert the frame into ascii then convert the ascii to ascii frame 
         ascii_frame = self.frame_to_ascii_to_ascii_image(frame)
         self.writer.write(ascii_frame) # save the frame 
 
-    def frame_thread_superviser(self):
-        print("working on image computing")
-        while not self.reader_completed :
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                new_frames = executor.map(self.frame_to_ascii_to_ascii_image , self.frame_list )
-                for new_frame in new_frames:
-                    Thread(target= lambda : self.frame_list.pop(0)).start()
-                    self.writer.write(new_frame) # save the frame 
-        
-        print('Done. 😎')
-
     @classmethod
     def runner(cls,video,output_video,fps,pbs):
         with cls(video,output_video,fps,pbs) as ascii_video :
-            reader = Thread(target= ascii_video.iter_each_frame )
-            reader.start()
-
-            # start the frame saving thread
-            saver = Thread(target = ascii_video.frame_thread_superviser)
-            saver.start()
-
-            # waiting for complete all the reading frames
-            reader.join()
-            print('waiting for the results...')
-            saver.join()
+            ascii_video.iter_each_frame()
 
 if __name__ == "__main__" : 
     start = perf_counter()
-    ascii_video.runner('ab.mp4',"Ascii_video2.mp4",30,10)
+    ascii_video.runner('ab.mp4',"Ascii_video1.mp4",30,10)
     finish = perf_counter()
-
     print(f"Total time Taken {finish - start} ")
